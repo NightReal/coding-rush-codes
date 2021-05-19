0e9256c9ce84022284835b26e1ec292e54fc2962
Alexander Nekrasov <alexspeedcuber@gmail.com>
2021-05-19 12:49:59 +0300

    Z-function added

---
+++ Strings/Z-function/codes/cpp.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> zFunction(string s) {
+	int n = s.size();
+	vector<int> z(n);
+	int l = 0;
+	int r = 0;
+	for (int i = 1; i < n; i++) {
+		if (i <= r) {
+			z[i] = min(r - i + 1, z[i - 1]);
+		}
+		while (i + z[i] < n && s[z[i]] = s[i + z[i]]) {
+			z[i]++;
+		}
+		if (i + z[i] - 1 > r) {
+			r = i + z[i] - 1;
+			l = i;
+		}
+	}
+	return z;
+}
+
+int main() {
+	
+}
