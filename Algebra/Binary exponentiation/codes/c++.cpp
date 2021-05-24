782dd7f86daff6c96dbbfd55664e9e1cb0f7f05d
Alexander Nekrasov <alexspeedcuber@gmail.com>
2021-05-24 14:35:12 +0300

    Fixed tabs

--- Algebra/Binary exponentiation/codes/c++.cpp
+++ Algebra/Binary exponentiation/codes/c++.cpp
@@ -1,11 +1,11 @@
 double binaryPow(double a, int n) {
-  double ans = 1;
-  while (n) {
-    if (n & 1) {
-      ans *= a;
-    }
-    n >>= 1;
-    a *= a;
-  }
-  return ans;
+	double ans = 1;
+	while (n) {
+		if (n & 1) {
+			ans *= a;
+		}
+		n >>= 1;
+		a *= a;
+	}
+	return ans;
 }
