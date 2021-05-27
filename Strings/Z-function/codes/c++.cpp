2402ec946fb8e632d99fc108cc063de4d96848ae
crazyilian <ilian.and.04@gmail.com>
2021-05-27 11:40:57 +0300

    [fix]: segment -> half-interval. Fixed bug in python code

--- Strings/Z-function/codes/c++.cpp
+++ Strings/Z-function/codes/c++.cpp
@@ -4,14 +4,14 @@ vector<int> zFunction(string s) {
 	int l = 0;
 	int r = 0;
 	for (int i = 1; i < n; i++) {
-		if (i <= r) {
-			z[i] = min(r - i + 1, z[i - l]);
+		if (i < r) {
+			z[i] = min(r - i, z[i - l]);
 		}
 		while (i + z[i] < n && s[z[i]] == s[i + z[i]]) {
 			z[i]++;
 		}
-		if (i + z[i] - 1 > r) {
-			r = i + z[i] - 1;
+		if (i + z[i] > r) {
+			r = i + z[i];
 			l = i;
 		}
 	}
--- Strings/Z-function/codes/python.py
+++ Strings/Z-function/codes/python.py
@@ -1,13 +1,13 @@
 def zFunction(s):
     n = len(s)
     l = r = 0
-    z = [0 for i in range(n)]
+    z = [0] * n
     for i in range(1, n):
-        if i <= r:
-            z[i] = min(r - i + l, z[i - l])
+        if i < r:
+            z[i] = min(r - i, z[i - l])
         while i + z[i] < n and s[z[i]] == s[i + z[i]]:
             z[i] += 1
-        if i + z[i] - 1 > r:
-            r = i + z[i] - 1
+        if i + z[i] > r:
+            r = i + z[i]
             l = i
     return z
