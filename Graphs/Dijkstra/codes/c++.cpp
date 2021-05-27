1539c0db8affeab873817cf0af2956a5915faccb
crazyilian <ilian.and.04@gmail.com>
2021-05-27 11:33:01 +0300

    [fix]: indent in empty lines

--- Graphs/Dijkstra/codes/c++.cpp
+++ Graphs/Dijkstra/codes/c++.cpp
@@ -11,11 +11,11 @@ void dijkstra(int s, vector<int> &d, vector<int> &p) {
 	while (!q.empty()) {
 		int v = q.begin()->second;
 		q.erase(q.begin());
-
+		
 		for (auto edge : adj[v]) {
 			int to = edge.first;
 			int len = edge.second;
-
+			
 			if (d[v] + len < d[to]) {
 				q.erase({d[to], to});
 				d[to] = d[v] + len;
